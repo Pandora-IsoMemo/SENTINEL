738de9432cfafb1a504db5de98c3058823a9c9e0
Jian Roachell <jianyin.roachell@gmail.com>
2023-11-05 00:17:02 -0400

    update nov 5

--- app/app.py
+++ app/app.py
@@ -7,29 +7,31 @@ update: Oct. 29, 2023
 
 
 import pandas as pd
-from shiny import App, Inputs, Outputs, Session, render, ui
+from shiny import App, Inputs, Outputs, Session, render, ui, reactive
 from shiny.types import FileInfo
 from matplotlib import pyplot as plt
 from helpers import *
 import io
+import webbrowser
+import shinyswatch
 
-series_may, ts_may = ews_exe()
+series_may, ts_may = get_example_df()
+
 
 app_ui = ui.page_fluid(
+
+    ui.include_css("/Users/johann/Documents/PROJECTS/SENTINEL/app/stylesheet.css"),
+
+    # Help buttons
+    #ui.div(
+    #    ui.input_action_button("help", "help_button_1", class_="btn btn-link"),
+        #ui.button("Help Button 2", id="help_button_2", class_="btn btn-link"),
+    #    class_="btn-group",
+    #    style={"margin-left": "10px", "margin-top": "10px"}),
     # TTILE PANEL
-    ui.panel_title("Sentinel v0.0.1"),
-#     ui.markdown("""Computing predictions of a DL classifier:
-# The method `apply_classifier()` applies a deep learning classifier (a TensorFlow Model) to a segment of the time series data, specified using tmin and tmax. The prediction (a vector of probabilites for each class) is then saved into the attribute dl_preds, which is a pandas DataFrame.
+    ui.panel_title("SENTINEL v0.0.1"),
+    ui.markdown("""Systematic Engagement with Time-series and Intelligent Early Learning"""),
 
-# Let's see this in action. We will import one of the classifiers that was trained in Bury et al. (PNAS, 2021). These are saved in the ewstools Github repository under /saved_classifiers for convenience. These classifiers were trained to predict the following classes:
-# > 0	fold bifurcation
-
-# > 1	Hopf bifurcation
-
-# > 2	transcritical bifurcation
-
-# > 3	null
-# """),
 
 
     # INPUTS
@@ -38,37 +40,54 @@ app_ui = ui.page_fluid(
     ui.layout_sidebar(
         ui.panel_sidebar(
             ui.input_file("file1", "Choose CSV File", accept=[".csv"], multiple=False),
+            ui.input_action_button("example_button", "Ex. Data", class_="btn-success"),
+
             ui.input_checkbox("header", "Header", True),
-            width=5,
+            ui.input_select("model_name", "Select Model", choices=model_names),
+            ui.input_action_button("model_button", "Model", class_="btn-success"),
+            width=4.5,
         ),
         ui.panel_main(
-            ui.output_ui("contents"),
+
+            # OUTPUTS
+            ui.markdown("""### Results"""),
+            ui.output_text("value"),
+            ui.output_table("contents"),
+            ui.markdown("""### Classifier Results"""),
+            ui.panel_conditional(
+                "input.model_button",
+                ui.output_table("ml_preds")
+            ),
+            ui.output_text_verbatim("txt"),
+            ui.output_plot("plot"),
+            ui.output_plot("plot2"),
+
         ),
     ),
 
-    # OUTPUTS
-    ui.output_table("ml_preds"),
-    ui.output_text_verbatim("txt"),
-    ui.output_plot("plot"),
-    ui.output_plot("plot2"),
+
 
 )
 
 def server(input, output, session):
     # Define a callback to read and process the uploaded file
+    # Define a callback to open the hyperlinks when help buttons are clicked
+
     @output
     @render.ui
     def contents():
         if input.file1() is None:
-            return "Please upload a csv file"
+            #print("Please upload a csv file OR click example data")
+            return ui.HTML(ts_may.state[['state', 'smoothing']].head().to_html(classes="table table-striped"))
+
         f: list[FileInfo] = input.file1()
         df = pd.read_csv(f[0]["datapath"], header=0 if input.header() else None)
-        return ui.HTML(df.to_html(classes="table table-striped"))
+        return ui.HTML(df.head().to_html(classes="table table-striped"))
 
     @output
-    @render.text()
-    def txt():
-        return f"n*2 is {input.n()}"
+    @render.text
+    def value():
+        return "You choose: " + str(input.model_name())
 
     @output
     @render.plot
@@ -82,8 +101,10 @@ def server(input, output, session):
 
     @output
     @render.table
+    @reactive.event(input.model_button, ignore_none=False)
     def ml_preds():
-        return ts_may.dl_preds
+        preds = get_model(str(input.model_name()), ts_may)
+        return preds
 
 
 
--- app/helpers.py
+++ app/helpers.py
@@ -9,14 +9,59 @@ tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR) # comment out to 
 from tensorflow.keras.models import load_model
 import ewstools
 from ewstools.models import simulate_may, simulate_rosen_mac
+from typing import List
+
+model_names = {
+"saved_classifiers/bury_pnas_21/len500/best_model_1_1_len500.pkl": "Model Lens500",
+"saved_classifiers/bury_pnas_21/len1500/best_model_1_1_len1500.pkl":"Model Lens1500"
+}
 
 
-def ews_exe():
+def get_example_df():
     series_may = simulate_may(tmax=500, dt=0.01, h=[0.15,0.28]).iloc[::100]
     ts_may = ewstools.TimeSeries(series_may['x'], transition=420)
     ts_may.detrend(method='Lowess', span=0.2)
     ts_may.state[['state','smoothing']].plot()
-    classifier_path = '/Users/johann/Documents/PROJECTS/sentinelpy/saved_classifiers/bury_pnas_21/len500/best_model_1_1_len500.pkl'
-    classifier = load_model(classifier_path)
-    ts_may.apply_classifier(classifier, tmin=0, tmax=400)
     return series_may, ts_may
+
+def get_model(model_path, ts_may):
+    classifier = load_model(model_path)
+    ts_may.apply_classifier(classifier, tmin=0, tmax=400)
+    return ts_may.dl_preds
+
+
+
+
+# UI helper
+from shiny.types import NavSetArg
+
+
+def nav_controls(prefix: str) -> List[NavSetArg]:
+    return [
+        ui.nav("a", prefix + ": tab a content"),
+        ui.nav("b", prefix + ": tab b content"),
+        ui.nav("c", prefix + ": tab c content"),
+        ui.nav_spacer(),
+        ui.nav_menu(
+            "Links",
+            ui.nav_control(
+                ui.a(
+                    "Shiny",
+                    href="https://shiny.posit.co/py/",
+                    target="_blank",
+                )
+            ),
+            "----",
+            "Plain text",
+            "----",
+            ui.nav_control(
+                ui.a(
+                    "Posit",
+                    href="https://posit.co",
+                    target="_blank",
+                )
+            ),
+            align="right",
+        ),
+    ]
+
